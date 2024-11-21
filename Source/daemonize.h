8d1eb34a538c0f6cb135e77e153f2e2ff605c329
lazybot2 <lazybot@qq.com>
2024-11-21 15:14:45 +0800

    make NanoHatOLED

---
+++ Source/daemonize.h
@@ -0,0 +1,46 @@
+/*
+NanoHat OLED example
+http://wiki.friendlyarm.com/wiki/index.php/NanoHat_OLED
+*/
+
+/*
+The MIT License (MIT)
+Copyright (C) 2017 FriendlyELEC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+
+#ifndef __DAEMONIZE__H__
+#define __DAEMONIZE__H__
+
+#define DEBUG           1
+#define LOG_FILE_NAME   "/tmp/nanohat-oled.log"
+#define LOCKFILE        "/var/run/nanohat-oled.pid"
+
+#define PYTHON3_INTERP  "python3.7"
+#define PYTHON3_SCRIPT  "lazybot.py"
+
+extern int isAlreadyRunning();
+extern void daemonize(const char *cmd);
+
+
+#endif
+
--- install.sh
+++ install.sh
@@ -21,14 +21,39 @@ esac
 usermod -a lazybot -G root
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install -y i2c-tools git wget vim python3-dev python3-pil python3-smbus python3-pip python3-serial 
+sudo apt-get install -y i2c-tools git wget vim gcc python3 python3-dev python3-pil python3-smbus python3-pip python3-serial 
 sudo pip3 install --upgrade setuptools
 sudo pip3 install sh
 sudo pip3 install wheel
 sudo pip3 install psutil
 sudo apt-get install -y  libc6 libncurses5 libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 libatk1.0-0 libgdk-pixbuf2.0-0 libglib2.0-0 libfontconfig1 \
  libfreetype6 libgtk-3-0 libusb-1.0-0 libplist3 usbmuxd ideviceinstaller python3-imobiledevice libimobiledevice-utils python3-plist ifuse libusbmuxd-tools \
- libjpeg-dev pkg-config libplist-dev libreadline-dev libusb-1.0-0-dev build-essential checkinstall autoconf automake libtool-bin
+ libjpeg-dev pkg-config libplist-dev libreadline-dev libusb-1.0-0-dev libssl-dev libffi-dev
+if [ ! -f /usr/bin/python3 ]; then
+    echo "/usr/bin/python3 not found, exiting."
+    exit 1
+fi
+
+PY3_INTERP=`readlink /usr/bin/python3`
+RET=$?
+if [ $? -ne 0 ]; then
+    echo "No executable python3, exiting."
+    exit 1
+fi
+REAL_PATH=$(realpath $(dirname $0))
+sed -i "/^#define.*PYTHON3_INTERP.*$/s/\".*\"/\"${PY3_INTERP}\"/" "${REAL_PATH}/Source/daemonize.h"
+
+echo ""
+echo "Compiling with GCC ..."
+echo "======================="
+gcc Source/daemonize.c Source/main.c -lrt -lpthread -o NanoHatOLED
+echo "Compiled NanoHatOLED"
+
+if [ ! -f $PWD/NanoHatOLED ]; then
+    echo "$PWD/NanoHatOLED not found, exiting."
+    exit 1
+fi
+
 if [ ! -f /usr/local/bin/oled-start ]; then
     cat >/usr/local/bin/oled-start <<EOL
 #!/bin/sh
@@ -43,24 +68,23 @@ case $nanopi in
     0)
     wget -O checkra1n https://assets.checkra.in/downloads/linux/cli/arm/ff05dfb32834c03b88346509aec5ca9916db98de3019adf4201a2a6efe31e9f5/checkra1n
     wget -O palera1n https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.7/palera1n-linux-armel
-    sudo rm -f ./NanoHatOLED-neo2
     ;;
     1)
     wget -O checkra1n https://assets.checkra.in/downloads/linux/cli/arm64/43019a573ab1c866fe88edb1f2dd5bb38b0caf135533ee0d6e3ed720256b89d0/checkra1n
     wget -O palera1n https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.7/palera1n-linux-arm64
-    sudo rm -f ./NanoHatOLED
-    mv ./NanoHatOLED-neo2 ./NanoHatOLED
     ;;
 esac
 sudo chmod +x ./NanoHatOLED
 sudo chmod +x ./checkra1n
 sudo chmod +x ./palera1n
-sudo apt-get install -y armbian-config=24.2.1
+sudo apt-get remove -y gcc
 du -h /var/cache/apt/archives
 sudo apt-get autoclean -y
 sudo apt-get clean -y
 sudo apt-get autoremove -y
-sudo armbian-config
+git clone https://github.com/armbian/config
+cd config
+bash debian-config
 
 
 
